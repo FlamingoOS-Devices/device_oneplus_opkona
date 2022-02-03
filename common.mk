ad3e97a6b43a9d018e2f2acac9d3bba8166439cb
johnmart19 <johnivan19999@gmail.com>
2022-02-03 22:14:41 +0200

    sm8250-common: Add android.hardware.media.c2@1.2 vendor version
    Signed-off-by: drkphnx <dark.phnx12@gmail.com>

--- common.mk
+++ common.mk
@@ -285,7 +285,9 @@ PRODUCT_PACKAGES += \
     libcodec2_vndk.vendor \
     libcodec2_hidl@1.0.vendor \
     libcodec2_hidl@1.0.vendor:32 \
-    libcodec2_vndk.vendor:32
+    libcodec2_vndk.vendor:32 \
+    android.hardware.media.c2@1.0.vendor \
+    android.hardware.media.c2@1.2.vendor
 
 # Net
 PRODUCT_PACKAGES += \
