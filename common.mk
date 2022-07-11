737abb91330faef12b31cf5121e72c5b7e673d23
HELLBOY017 <abhaygill017@gmail.com>
2022-07-11 19:54:44 +0530

    sm8250-common: overlay: Setup overlays for zephyrus

--- common.mk
+++ common.mk
@@ -326,7 +326,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-zep
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
