4502f07dea6233a1f99906bbcb4f46ac8ca9b37f
chandu078 <chandu078@users.noreply.github.com>
2022-08-14 10:27:53 +0530

    sm8250-common: init: Set device, name & model props according to PRJ and RF v…
    …ersion
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- libinit/init_oplus.cpp
+++ libinit/init_oplus.cpp
@@ -7,173 +7,246 @@
 #include <android-base/properties.h>
 
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/sysinfo.h>
+#include <sys/system_properties.h>
 #include <sys/_system_properties.h>
 
+#include "property_service.h"
+#include "vendor_init.h"
+
 using android::base::GetProperty;
+using std::string;
+
+std::vector<std::string> ro_props_default_source_order = {
+    "",
+    "odm.",
+    "product.",
+    "system.",
+    "system_ext.",
+    "vendor.",
+    "vendor_dlkm."
+};
 
 /*
  * SetProperty does not allow updating read only properties and as a result
- * does not work for our use case. Write "OverrideProperty" to do practically
+ * does not work for our use case. Write "property_override" to do practically
  * the same thing as "SetProperty" without this restriction.
  */
-void OverrideProperty(const char* name, const char* value) {
-    size_t valuelen = strlen(value);
 
-    prop_info* pi = (prop_info*)__system_property_find(name);
-    if (pi != nullptr) {
-        __system_property_update(pi, value, valuelen);
-    } else {
-        __system_property_add(name, strlen(name), value, valuelen);
+void property_override(char const prop[], char const value[], bool add = true) {
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else if (add)
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void set_ro_build_prop(const std::string &prop, const std::string &value, bool product = true) {
+    string prop_name;
+
+    for (const auto &source : ro_props_default_source_order) {
+        if (product)
+            prop_name = "ro.product." + source + prop;
+        else
+            prop_name = "ro." + source + "build." + prop;
+
+        property_override(prop_name.c_str(), value.c_str());
     }
 }
+
+void vendor_load_properties() {
+
+  string model;
+  string device;
+  string name;
 
 /*
  * Only for read-only properties. Properties that can be wrote to more
  * than once should be set in a typical init script (e.g. init.oplus.hw.rc)
  * after the original property has been set.
  */
-void vendor_load_properties() {
-    auto prj_version = std::stoi(GetProperty("ro.boot.prj_version", "0"));
-    auto rf_version = std::stoi(GetProperty("ro.boot.rf_version", "0"));
 
-    switch (prj_version) {
-        case 19821:
-        OverrideProperty("ro.vendor.usb.name", "OnePlus 8");
-      /* OnePlus 8 */
+  auto prj_version = std::stoi(GetProperty("ro.boot.prj_version", "0"));
+  auto rf_version = std::stoi(GetProperty("ro.boot.rf_version", "0"));
+
+  switch(prj_version){
+  	/* OnePlus 8 */
+    case 19821:
+	device = "OnePlus8";
+	property_override("ro.vendor.usb.name", "OnePlus 8");
       switch (rf_version){
-        case 11:
           /* China */
-          OverrideProperty("ro.product.model", "IN2010");
+        case 11:
+          name = "OnePlus8";
+          model = "IN2010";
           break;
         case 13:
           /* India */
-          OverrideProperty("ro.product.model", "IN2011");
+          name = "OnePlus8_IND";
+          model = "IN2011";
           break;
         case 14:
           /* Europe */
-          OverrideProperty("ro.product.model", "IN2013");
+          name = "OnePlus8_EEA";
+          model = "IN2013";
           break;
         case 15:
           /* Global / US Unlocked */
-          OverrideProperty("ro.product.model", "IN2015");
+          name = "OnePlus8";
+          model = "IN2015";
           break;
         default:
           /* Generic */
-          OverrideProperty("ro.product.model", "IN2015");
+          name = "OnePlus8";
+          model = "IN2015";
           break;
       }
       break;
+        /* OnePlus 8 T-Mobile */
     case 19855:
-    OverrideProperty("ro.vendor.usb.name", "OnePlus 8");
-      /* OnePlus 8 T-Mobile */
+	device = "OnePlus8";
+	property_override("ro.vendor.usb.name", "OnePlus 8");
       switch (rf_version){
+            /* T-Mobile */
         case 12:
-          /* T-Mobile */
-          OverrideProperty("ro.product.model", "IN2017");
+          name = "OnePlus8_TMO";
+          model = "IN2017";
           break;
+            /* Generic */
         default:
-          /* Generic */
-          OverrideProperty("ro.product.model", "IN2015");
+          name = "OnePlus8";
+          model = "IN2015";
           break;
       }
       break;
+        /* OnePlus 8 Verizon */
     case 19867:
-    OverrideProperty("ro.vendor.usb.name", "OnePlus 8");
-      /* OnePlus 8 Verizon */
+	device = "OnePlus8";
+	property_override("ro.vendor.usb.name", "OnePlus 8");
       switch (rf_version){
+            /* T-Mobile */
         case 25:
-          /* Verizon */
-          OverrideProperty("ro.product.model", "IN2019");
+          name = "OnePlus8";
+          model = "IN2019";
           break;
+            /* Generic */
         default:
-          /* Generic */
-          OverrideProperty("ro.product.model", "IN2015");
+          name = "OnePlus8";
+          model = "IN2015";
           break;
       }
       break;
+        /* OnePlus 8 Pro */
     case 19811:
-    OverrideProperty("ro.vendor.usb.name", "OnePlus 8 Pro");
-      /* OnePlus 8 Pro */
+	device = "OnePlus8Pro";
+	property_override("ro.vendor.usb.name", "OnePlus 8 Pro");
       switch (rf_version){
-        case 11:
           /* China */
-          OverrideProperty("ro.product.model", "IN2020");
+        case 11:
+          name = "OnePlus8Pro";
+          model = "IN2020";
           break;
         case 13:
           /* India */
-          OverrideProperty("ro.product.model", "IN2021");
+          name = "OnePlus8Pro_IND";
+          model = "IN2021";
           break;
         case 14:
           /* Europe */
-          OverrideProperty("ro.product.model", "IN2023");
+          name = "OnePlus8Pro_EEA";
+          model = "IN2023";
           break;
         case 15:
           /* Global / US Unlocked */
-          OverrideProperty("ro.product.model", "IN2025");
+          name = "OnePlus8Pro";
+          model = "IN2025";
           break;
         default:
           /* Generic */
-          OverrideProperty("ro.product.model", "IN2025");
+          name = "OnePlus8Pro";
+          model = "IN2025";
           break;
       }
       break;
-      case 19805:
-      OverrideProperty("ro.vendor.usb.name", "OnePlus 8T");
-      /* OnePlus 8T */
+       /* OnePlus 8T */
+    case 19805:
+	device = "OnePlus8T";
+	property_override("ro.vendor.usb.name", "OnePlus 8T");
       switch (rf_version){
+           /* China */
         case 11:
-          /* China */
-          OverrideProperty("ro.product.model", "KB2000");
+          name = "OnePlus8T";
+          model = "KB2000";
           break;
+            /* India */
         case 13:
-          /* India */
-          OverrideProperty("ro.product.model", "KB2001");
+          name = "OnePlus8T_IND";
+          model = "KB2001";
           break;
+            /* Europe */
         case 14:
-          /* Europe */
-          OverrideProperty("ro.product.model", "KB2003");
+          name = "OnePlus8T_EEA";
+          model = "KB2003";
           break;
+            /* Global / US Unlocked */
         case 15:
-          /* Global / US Unlocked */
-          OverrideProperty("ro.product.model", "KB2005");
+          name = "OnePlus8T";
+          model = "KB2005";
           break;
+            /* Generic */
         default:
-          /* Generic */
-          OverrideProperty("ro.product.model", "KB2005");
+          name = "OnePlus8T";
+          model = "KB2005";
           break;
       }
       break;
+       /* OnePlus 8T T-Mobile */
     case 20809:
-    OverrideProperty("ro.vendor.usb.name", "OnePlus 8T");
-      /* OnePlus 8T T-Mobile */
+	device = "OnePlus8T";
+	property_override("ro.vendor.usb.name", "OnePlus 8T");
       switch (rf_version){
+            /* T-Mobile */
         case 12:
-          /* T-Mobile */
-          OverrideProperty("ro.product.model", "KB2007");
+          name = "OnePlus8T_TMO";
+          model = "KB2007";
           break;
+            /* Generic */
         default:
-          /* Generic */
-          OverrideProperty("ro.product.model", "KB2005");
+          name = "OnePlus8T";
+          model = "KB2005";
           break;
       }
       break;
-     case 20828:
-      /* OnePlus 9R */
-      OverrideProperty("ro.vendor.usb.name", "OnePlus 9R");
+      	/* OnePlus 9R */
+   case 20828:
+	device = "OnePlus9R";
+	property_override("ro.vendor.usb.name", "OnePlus 9R");
       switch (rf_version){
+            /* China */
       case 11:
-          /* China */
-          OverrideProperty("ro.product.model", "LE2100");
+          name = "OnePlus9R";
+          model = "LE2100";
           break;
+            /* India */
       case 13:
-          /* India */
-          OverrideProperty("ro.product.model", "LE2101");
+          name = "OnePlus9R_IND";
+          model = "LE2101";
           break;
+            /* Generic */
       default:
-          /* Generic */
-          OverrideProperty("ro.product.model", "LE2101");
+          name = "OnePlus9R_IND";
+          model = "LE2101";
           break;
       }
       break;
-    }
+   }
+
+    set_ro_build_prop("device", device);
+    set_ro_build_prop("model", model);
+    set_ro_build_prop("name", name);
+    set_ro_build_prop("product", model, false);
 }
