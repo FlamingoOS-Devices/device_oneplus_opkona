Project-Zephyrus-Devices/device_oneplus_sm8250-common

sm8250-common: Introduce DeviceExtras! - Awesome alert slider configs - Ambient display gestures (doze merged into DeviceExtras) + QS tile - FPS Overlay + QS tile - HBM & Auto HBM + QS tile - Force GameMode (240hz touch sampling) toggle + QS - Vibration strength preference
Co-authored-by: Max Weninger <max.weninger@gmail.com>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: AliHasan7671 <AHB7671@gmail.com>
Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
Co-authored-by: darkobas <darkobas@gmail.com>
Co-authored-by: kldoc <fred.berbigier@gmail.com>
Co-authored-bt: Nauval Rizky <enuma.alrizky@gmail.com>
Co-authored-by: Christian Oder <myself5@carbonrom.org>
Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
Co-authored-by: LuK1337 <priv.luk@gmail.com>
Co-authored-by: chrisw444 <wandersonrodriguesf1@gmail.com>
Co-authored-by: Adrian DC <radian.dc@gmail.com>
Co-authored-by: SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
Co-authored-by: Michele Bono <bono.michele94@gmail.com>
Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
Co-authored-by: Ziauddin Sameer <ziauddinsameer@gmail.com>
Signed-off-by: HELLBOY017 <abhaygill017@gmail.com>

DeviceExtras/src/org/zephyrus/device/DeviceExtras/ModeSwitch/GameModeSwitch.java
@@ -1,0 +1,52 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.zephyrus.device.DeviceExtras;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import org.zephyrus.device.DeviceExtras.DeviceExtras;
+
+public class GameModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/proc/touchpanel/game_switch_enable";
+
+    public static String getFile() {
+        if (FileUtils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return FileUtils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        FileUtils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceExtras/src/org/zephyrus/device/DeviceExtras/Startup.java
@@ -1,0 +1,71 @@
+/*
+* Copyright (C) 2013 The OmniROM Project
+* Copyright (C) 2021 The dot OS Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.zephyrus.device.DeviceExtras;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import androidx.preference.PreferenceManager;
+
+public class Startup extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, final Intent bootintent) {
+
+        boolean enabled = false;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_HBM_SWITCH, false);
+        if (enabled) {
+        restore(HBMModeSwitch.getFile(), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_FPS_INFO, false);
+        if (enabled) {
+            context.startService(new Intent(context, FPSInfoService.class));
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_GAME_SWITCH, false);
+        if (enabled) {
+        restore(GameModeSwitch.getFile(), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_EDGE_TOUCH, true);
+        if (enabled) {
+        restore(EdgeTouchSwitch.getFile(), enabled);
+        }
+        DeviceExtras.restoreSliderStates(context);
+        org.zephyrus.device.DeviceExtras.doze.DozeUtils.checkDozeService(context);
+        VibratorStrengthPreference.restore(context);
+    }
+
+    private void restore(String file, boolean enabled) {
+        if (file == null) {
+            return;
+        }
+        if (enabled) {
+            FileUtils.writeValue(file, "1");
+        }
+    }
+
+    private void restore(String file, String value) {
+        if (file == null) {
+            return;
+        }
+        FileUtils.writeValue(file, value);
+    }
+}

FlamingoOS-Devices/device_oneplus_opkona

DeviceExtras/src/org/zephyrus/device/DeviceExtras/ModeSwitch/GameModeSwitch.java
@@ -1,0 +1,52 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.zephyrus.device.DeviceExtras;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import org.zephyrus.device.DeviceExtras.FileUtils;
+
+public class GameModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/proc/touchpanel/game_switch_enable";
+
+    public static String getFile() {
+        if (FileUtils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return FileUtils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return FileUtils.getFileValueAsBoolean(getFile(), false, "1, smooth_level:0, sensitive_level:0, high_frame_support:0, high_frame_value:0", "0, smooth_level:0, sensitive_level:0, high_frame_support:0, high_frame_value:0");
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        FileUtils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceExtras/src/org/zephyrus/device/DeviceExtras/Startup.java
@@ -1,0 +1,76 @@
+/*
+* Copyright (C) 2013 The OmniROM Project
+* Copyright (C) 2021 The dot OS Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.zephyrus.device.DeviceExtras;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import androidx.preference.PreferenceManager;
+
+public class Startup extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, final Intent bootintent) {
+
+        boolean enabled = false;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_HBM_SWITCH, false);
+        if (enabled) {
+        restore(HBMModeSwitch.getFile(), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_DC_SWITCH, false);
+        if (enabled) {
+        restore(DCModeSwitch.getFile(), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_FPS_INFO, false);
+        if (enabled) {
+            context.startService(new Intent(context, FPSInfoService.class));
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_GAME_SWITCH, false);
+        if (enabled) {
+        restore(GameModeSwitch.getFile(), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_OTG_SWITCH, false);
+        if (enabled) {
+        restore(OTGModeSwitch.getFile(), enabled);
+               }
+        AdrenoBoostPreference.restore(context);
+        DeviceExtras.restoreSliderStates(context);
+        org.zephyrus.device.DeviceExtras.doze.DozeUtils.checkDozeService(context);
+        VibratorStrengthPreference.restore(context);
+    }
+
+    private void restore(String file, boolean enabled) {
+        if (file == null) {
+            return;
+        }
+        if (enabled) {
+            FileUtils.writeValue(file, "1");
+        }
+    }
+
+    private void restore(String file, String value) {
+        if (file == null) {
+            return;
+        }
+        FileUtils.writeValue(file, value);
+    }
+}
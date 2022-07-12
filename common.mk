c41afbe86396d072664498a6a52654a5aeeb8795
AnierinBliss <AnierinBliss@users.noreply.github.com>
2022-07-12 15:37:40 +0530

    sm8250-common: Introduce DeviceExtras! - Awesome alert slider configs - Ambient display gestures (doze merged into DeviceExtras) + QS tile - FPS Overlay + QS tile - HBM & Auto HBM + QS tile - Force GameMode (240hz touch sampling) toggle + QS - Vibration strength preference
    Co-authored-by: Max Weninger <max.weninger@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: AliHasan7671 <AHB7671@gmail.com>
    Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: kldoc <fred.berbigier@gmail.com>
    Co-authored-bt: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-authored-by: Christian Oder <myself5@carbonrom.org>
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Co-authored-by: Adrian DC <radian.dc@gmail.com>
    Co-authored-by: SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
    Co-authored-by: Michele Bono <bono.michele94@gmail.com>
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
    Co-authored-by: Ziauddin Sameer <ziauddinsameer@gmail.com>
    Signed-off-by: HELLBOY017 <abhaygill017@gmail.com>

--- common.mk
+++ common.mk
@@ -157,6 +157,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     com.android.media.swcodec \
     libsfplugin_ccodec
+    
+# DeviceExtras
+PRODUCT_PACKAGES += \
+    DeviceExtras \
+    tri-state-key-calibrate
 
 # Display
 PRODUCT_PACKAGES += \
@@ -196,10 +201,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
-
-# Doze
-PRODUCT_PACKAGES += \
-    OplusDoze
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -391,7 +392,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
-    TelephonyResCommon \
     WifiResCommon \
     OPlusWifiResCommon \
     WifiResTarget \
@@ -512,7 +512,8 @@ PRODUCT_PACKAGES += \
     
 # Touch
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.oplus
+    vendor.lineage.touch@1.0-service.oplus \
+    TouchGestures
 
 # Update engine
 PRODUCT_PACKAGES += \
