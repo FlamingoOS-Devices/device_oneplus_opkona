13ba099947e6ae13b3999c8b5e61d63dce27d26f
HELLBOY017 <abhaygill017@gmail.com>
2022-10-18 16:15:55 +0530

    sm8250-common: Remove Chromium and GalleryGo packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := GalleryGoPrebuilt Gallery2 SafetyHubPrebuilt MusicFX RecorderPrebuilt Snap
+LOCAL_OVERRIDES_PACKAGES := GalleryGo Gallery2 SafetyHubPrebuilt MusicFX RecorderPrebuilt Snap Chromium
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
